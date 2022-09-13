4a6d684037b84c072df3cd5293182d3b15c28501
CJ Edgecombe <cjledgecombe@gmail.com>
2022-09-13 17:44:16 -0600

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,38 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // create variable that contains the current queue size + 1
+    var newKey = (someInstance.size(storage)) + 1;
+    // add input value to queue with the key being the result of the above variable
+    storage[newKey] = value;
   };
 
   someInstance.dequeue = function() {
+    // create var that contains the value of the property at the front of the queue
+    var frontOfQueue = storage[1];
+    // delete the property at the front of the queue
+    delete storage[1];
+    // iterate through object, reassigning values to their key - 1
+    for (var key in storage) {
+      var keyMinusOne = Number(key) - 1;
+
+      storage[keyMinusOne] = storage[key];
+      // once new property has been created, delete the old one
+      delete storage[key];
+    }
+    // return front of queue value
+    return frontOfQueue;
   };
 
   someInstance.size = function() {
+    // create queueSize variable, set to zero
+    var queueSize = 0;
+    // iterate through storage, incrementing count for each property
+    for (var key in storage) {
+      queueSize++;
+    }
+    // return count
+    return queueSize;
   };
 
   return someInstance;
