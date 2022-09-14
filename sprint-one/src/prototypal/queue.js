553bd6179ca6497c6b56c8924cf25863691f1b49
CJ Edgecombe <cjledgecombe@gmail.com>
2022-09-14 17:03:25 -0600

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,48 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // use prototypal instantiation to create an instance that references queueMethods
+  var someInstance = Object.create(queueMethods);
+  // add a storage key to instance, set to empty object
+  someInstance.storage = {};
+  // return instance
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    // create variable that contains current queue size + 1
+    var newKey = this.size(this.storage) + 1;
+    // assign value to key at above variable
+    this.storage[newKey] = value;
+  },
+
+  dequeue: function () {
+    storage = this.storage;
+    // create variable that references value at the front of the queue
+    var frontOfQueue = storage[1];
+    // delete the property at the front of the queue
+    delete storage[1];
+    // iterate through storage, assign current value to key - 1
+    for (var key in storage) {
+      var keyMinusOne = Number(key) - 1;
+
+      storage[keyMinusOne] = storage[key];
+      // delete current value
+      delete storage[key];
+    }
+    // return value previously at the front of the queue
+    return frontOfQueue;
+  },
+
+  size: function () {
+    // create queueSize variable, set to zero
+    var queueSize = 0;
+    // iterate through storage, incrementing count for each property
+    for (var key in this.storage) {
+      queueSize++;
+    }
+    // return count
+    return queueSize;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,43 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // use prototypal instantiation to create an instance that references the stackMethods obj
+  var someInstance = Object.create(stackMethods);
+  // add a storage key to the instance with the value of an empty object
+  someInstance.storage = {};
+  // return the instancez
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    var storage = this.storage;
+    // create newKey variable, assign to value of current storage size + 1
+    var newKey = (this.size(storage)) + 1;
+    // add value to storage with newKey as its key
+    storage[newKey] = value;
+  },
+
+  pop: function () {
+    var storage = this.storage;
+    // create topOfStack variable, set to current size of storage
+    var topOfStack = this.size(storage);
+    // create variable to store value at top of stack
+    var value = storage[topOfStack];
+    // delete proptery at top of stack
+    delete storage[topOfStack];
+    // return stored value
+    return value;
+  },
+
+  size: function () {
+    // create stackSize variable, set to 0
+    var stackSize = 0;
+    // iterate through storage object, incrementing stackSize for each property
+    for (var key in this.storage) {
+      stackSize++;
+    }
+    // return stackSize
+    return stackSize;
+  }
+};
 
 
