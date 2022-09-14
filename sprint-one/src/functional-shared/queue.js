8eb49f94f1160a38305a8da112ddff8457a8ff38
CJ Edgecombe <cjledgecombe@gmail.com>
2022-09-13 20:55:17 -0600

    Complete functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,47 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {}
+  };
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    // create variable that contains current queue size + 1
+    var newKey = this.size(this.storage) + 1;
+    // assign value to key at above variable
+    this.storage[newKey] = value;
+  },
 
+  dequeue: function () {
+    storage = this.storage;
+    // create variable that references value at the front of the queue
+    var frontOfQueue = storage[1];
+    // delete the property at the front of the queue
+    delete storage[1];
+    // iterate through storage, assign current value to key - 1
+    for (var key in storage) {
+      var keyMinusOne = Number(key) - 1;
 
+      storage[keyMinusOne] = storage[key];
+      // delete current value
+      delete storage[key];
+    }
+    // return value previously at the front of the queue
+    return frontOfQueue;
+  },
+
+  size: function () {
+    // create queueSize variable, set to zero
+    var queueSize = 0;
+    // iterate through storage, incrementing count for each property
+    for (var key in this.storage) {
+      queueSize++;
+    }
+    // return count
+    return queueSize;
+  }
+};
