5739da9795027b438afe7996b1823b4ef98ce316
CJ Edgecombe <cjledgecombe@gmail.com>
2022-09-14 21:37:06 -0600

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,51 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create storage property on the instance to be
+  this.storage = {};
 };
 
 
+// Enqueue
+Queue.prototype.enqueue = function(value) {
+  // create a storage alias
+  var storage = this.storage;
+  // create newKey variable to reference current size of queue + 1
+  var newKey = this.size(storage) + 1;
+  // assign input value to storage with a key of the newKey value
+  storage[newKey] = value;
+};
+
+
+// Dequeue
+Queue.prototype.dequeue = function () {
+  // create storage alias
+  var storage = this.storage;
+  // create variable that references value at front of queue
+  var frontOfQueue = storage[1];
+  // delete property at front of queue
+  delete storage[1];
+  // iterate through storage properties
+  for (var key in storage) {
+    // create alias for the value of the current key - 1
+    var keyMinusOne = Number(key) - 1;
+    // reassign current value to its key - 1
+    storage[keyMinusOne] = storage[key];
+    // delete current (old) property
+    delete storage[key];
+  }
+  // return value that was until recently at the front of queue
+  return frontOfQueue;
+};
+
+// Size
+Queue.prototype.size = function() {
+  // create variable to store the count of the queue
+  var queueSize = 0;
+  // iterate over the storage property of the instance, incrementing the count
+  for (var key in this.storage) {
+    queueSize++;
+  }
+  // return the count
+  return queueSize;
+};
+
+var lineAtDisneyWorld = new Queue;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,44 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // add a storage property to the instance that will be
+  this.storage = {};
 };
 
 
+// Push
+Stack.prototype.push = function(value) {
+  // create storage alias
+  storage = this.storage;
+  // create newKey variable that references current size of stack + 1
+  var newKey = this.size(storage) + 1;
+  // assign input as a value to stack with newKey as its key
+  storage[newKey] = value;
+};
+
+// Pop
+Stack.prototype.pop = function() {
+  // create storage alias
+  var storage = this.storage;
+  // create top of stack variable that references the stack size
+  var topOfStack = this.size(storage);
+  // create variable to reference the top stack value
+  var topValue = storage[topOfStack];
+  // delete the property at the top of the stack
+  delete storage[topOfStack];
+  // return deleted value
+  return topValue;
+};
+
+
+// Size
+Stack.prototype.size = function () {
+  // create a variable to store a stack count
+  var stackSize = 0;
+  // iterate through object, incrementing stack count for each property
+  for (var key in this.storage) {
+    stackSize++;
+  }
+  // return stack count
+  return stackSize;
+};
+
+var pancakes = new Stack;
