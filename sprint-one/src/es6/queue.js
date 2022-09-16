8c3dae00702439c0d491d4c20b691efeb901985b
CJ Edgecombe <cjledgecombe@gmail.com>
2022-09-16 17:47:58 -0600

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,53 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    // add storage object
+    this.storage = {};
+  }
+
+  // Enqueue
+  enqueue(value) {
+    // create storage alias
+    var storage = this.storage;
+    // create newKey variable that references the size of storage + 1
+    var newKey = this.size(storage) + 1;
+    // add input value to queue with newKey as the key
+    storage[newKey] = value;
+  }
+
+
+  // Dequeue
+  dequeue() {
+    // create storage alias
+    var storage = this.storage;
+    // create variable that references the value at the front of the queue
+    var frontOfQueue = storage[1];
+    // delete the property at the front of the queue
+    delete storage[1];
+    // iterate through the queue
+    for (var key in storage) {
+      // re-assign each value to its key - 1
+      storage[(key - 1)] = storage[key];
+      // delete the old property
+      delete storage[key];
+    }
+    // return value that was until recently at the front of the queue
+    return frontOfQueue;
+  }
+
+
+  // Size
+  size() {
+    // create count variable
+    var queueSize = 0;
+    // iterate through storage, incrementing count for each property
+    for (var key in this.storage) {
+      queueSize++;
+    }
+    // return count
+    return queueSize;
   }
 
 }
+
+var lineAtTheDMV = new Queue;
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,48 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    // add storage variable set to an empty object
+    this.storage = {};
+  }
+
+  // Push
+  push(value) {
+    // create storage alias
+    var storage = this.storage;
+    // create a newKey variable that references the current stack size + 1
+    var newKey = this.size(storage) + 1;
+    // add input value to stack with newKey as the key
+    storage[newKey] = value;
+  }
+
+
+  // Pop
+  pop() {
+    // create storage alias
+    var storage = this.storage;
+    // create variable that references the current stack size
+    var topOfStack = this.size(storage);
+    // create variable that references the value at the top of the stack
+    var topValue = storage[topOfStack];
+    // delete the property at the top of the stack
+    delete storage[topOfStack];
+    // return value that was until recently at the top of the stack
+    return topValue;
+  }
+
+
+  // Size
+  size () {
+    // create count variable
+    var stackSize = 0;
+    // iterate through storage, incrementing count for each property
+    for (var key in this.storage) {
+      stackSize++;
+    }
+    // return count
+    return stackSize;
   }
 
 }
+
+var paperwork = new Stack;
